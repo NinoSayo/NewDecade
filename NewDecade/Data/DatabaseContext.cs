931d1c61c034c722c618a5591b95e1a1930f6b29
NinoSayo <119102188+NinoSayo@users.noreply.github.com>
2024-01-16 16:41:22 +0700

    Add files via upload

---
+++ NewDecade/Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewDecade.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> o) : base(o) { }
+
+        DbSet
+    }
+}
