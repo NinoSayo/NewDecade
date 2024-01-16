9e4f20d970bdc7aeb6c30683cad62c7f81b303bd
NinoSayo <dphat1120@gmail.com>
2024-01-16 17:18:41 +0700

    Main

---
+++ NewDecade/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewDecade.Models
+{
+    public class Users
+    {
+        [Key]
+        public string Id { get; set; } 
+        public string Username { get; set; }
+        [MaxLength(12)]
+        public string Password {  get; set; }
+        [MaxLength(12)]
+        public int Phone {  get; set; }
+    }
+}
--- NewDecade/Program.cs
+++ NewDecade/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NewDecade.Data;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,8 +13,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DatabaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(op =>
+{
+    op.RequireHttpsMetadata = false;
+    op.SaveToken = true;
+    op.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
