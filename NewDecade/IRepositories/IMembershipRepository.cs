6211cfac5cd3549f443b6986a2f51add35996adf
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-01-19 18:41:46 +0700

    Add files via upload

---
+++ NewDecade/IRepositories/IMembershipRepository.cs
@@ -0,0 +1,11 @@
+﻿using NewDecade.Models;
+
+namespace NewDecade.IRepositories
+{
+    public interface IMembershipRepository
+    {
+        Task<int> RegisterMembership(int userId, int packageId);
+        Task<IEnumerable<Membership>> GetAllPackages();
+        Task<Membership> GetPackageById(int packageId);
+    }
+}
--- NewDecade/IRepositories/IOrderRepository.cs
+++ NewDecade/IRepositories/IOrderRepository.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿// Trong Repositories/IOrderRepository.cs
 using NewDecade.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace NewDecade.IRepositories
+namespace NewDecade.Repositories
 {
-	public interface IOrderRepository
-	{
-		Task<Order> OrderNextStep(int orderId);
-	}
-}
+    public interface IOrderRepository
+    {
+        Task<int> AddOrder(Order order);
+        Task<int> DeleteOrder(int Id);
+        Task<IEnumerable<Order>> GetAllOrders(int userId);  
+        Task<Order> GetOrderDetails(int userId);
+        Task<int> AddItemToOrder(int orderId, Item item);
+        Task<int> UpdateDeliveryStatus(int orderId, string deliveryStatus);
+        Task<int> UpdatePaymentStatus(int orderId, string paymentStatus);
+        Task<int> UpdateRemainingAmount(int orderId, int itemId, decimal remainingAmount);
+        Task<int> UpdateItemDetails(int orderId, int itemId, decimal weight, string paymentType);
+        Task<decimal> CalculateLaundryPayment(int orderId, Item item);
+        Task<decimal> CalculateWeightPayment(int orderId, Item item);
 
+    }
+}
