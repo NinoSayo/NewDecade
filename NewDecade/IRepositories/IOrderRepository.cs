986d89502c16b98a2a875d9b1cab9161d5774e11
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-01-23 05:46:10 +0700

    Add files via upload

--- NewDecade/IRepositories/IOrderRepository.cs
+++ NewDecade/IRepositories/IOrderRepository.cs
@@ -1,23 +1,13 @@
-﻿// Trong Repositories/IOrderRepository.cs
-using NewDecade.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using ReactServer.Models;
 
-namespace NewDecade.Repositories
+namespace ReactServer.IRepository
 {
     public interface IOrderRepository
     {
-        Task<int> AddOrder(Order order);
-        Task<int> DeleteOrder(int Id);
-        Task<IEnumerable<Order>> GetAllOrders(int userId);  
-        Task<Order> GetOrderDetails(int userId);
-        Task<int> AddItemToOrder(int orderId, Item item);
-        Task<int> UpdateDeliveryStatus(int orderId, string deliveryStatus);
-        Task<int> UpdatePaymentStatus(int orderId, string paymentStatus);
-        Task<int> UpdateRemainingAmount(int orderId, int itemId, decimal remainingAmount);
-        Task<int> UpdateItemDetails(int orderId, int itemId, decimal weight, string paymentType);
-        Task<decimal> CalculateLaundryPayment(int orderId, Item item);
-        Task<decimal> CalculateWeightPayment(int orderId, Item item);
-
+        Task<IEnumerable<Order>> GetOrder();
+        Task<Order> GetOrderById(int orderid);
+        Task<Order> AddOrder(Order order);
+        Task<Order> UpdateOrder(Order order);
+        Task<bool> DeleteOrder(int orderid);
     }
 }
---
+++ NewDecade/IRepositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReactServer.Models;
+
+namespace ReactServer.IRepository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(int id);
+        Task<Product> AddProduct(Product product);  
+        Task<Product> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(int id);
+    }
+}
