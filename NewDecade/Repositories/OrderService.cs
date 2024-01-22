b48aca72d21fffeeac98df12c154ccebc31e2ddc
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-01-23 05:47:57 +0700

    Add files via upload

---
+++ NewDecade/Repositories/OrderService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ReactServer.Data;
+using ReactServer.IRepository;
+using ReactServer.Models;
+
+namespace ReactServer.Services
+{
+    public class OrderService : IOrderRepository
+    {
+        private readonly DatabaseContext db;
+
+        public OrderService(DatabaseContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Order> AddOrder(Order order)
+        {
+            // Thêm đơn hàng vào cơ sở dữ liệu
+            db.Orders.Add(order);
+            await db.SaveChangesAsync();
+
+            if (order.LaundryType == "GiatTheoBoQuanAo")
+            {
+                order.TotalAmount = order.Quantity * 20000;
+            }
+           
+            await db.SaveChangesAsync();
+
+            return order;
+        }
+
+        public async Task<bool> DeleteOrder(int orderId)
+        {
+            var orderToDelete = await db.Orders.FindAsync(orderId);
+
+            if (orderToDelete == null)
+            {
+                return false; // Đơn đặt hàng không tìm thấy
+            }
+
+            db.Orders.Remove(orderToDelete);
+            await db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrder()
+        {
+            return await db.Orders.ToListAsync();
+        }
+
+        public async Task<Order> GetOrderById(int orderid)
+        {
+            return await db.Orders.FindAsync(orderid);
+        }
+
+        public async Task<Order> UpdateOrder(Order order)
+        {
+            var existingOrder = await db.Orders.FindAsync(order.OrderId);
+
+            if (existingOrder != null)
+            {
+                existingOrder.FullName = order.FullName;
+                existingOrder.Address = order.Address;
+                existingOrder.Phone = order.Phone;
+                existingOrder.LaundryType = order.LaundryType;
+
+                await db.SaveChangesAsync();
+            }
+
+            return existingOrder;
+        }
+    }
+}
---
+++ NewDecade/Repositories/ProductSerivce.cs
@@ -0,0 +1,69 @@
+﻿// ProductSerivce.cs
+using Microsoft.EntityFrameworkCore;
+using ReactServer.Data;
+using ReactServer.IRepository;
+using ReactServer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReactServer.Services
+{
+    public class ProductSerivce : IProductRepository
+    {
+        private readonly DatabaseContext db;
+
+        public ProductSerivce(DatabaseContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Product> AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            var productToDelete = await db.Products.FindAsync(id);
+
+            if (productToDelete == null)
+            {
+                return false; // Product not found
+            }
+
+            db.Products.Remove(productToDelete);
+            await db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await db.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await db.Products.ToListAsync();
+        }
+
+        public async Task<Product> UpdateProduct(Product updatedProduct)
+        {
+            var existingProduct = await db.Products.FindAsync(updatedProduct.Id);
+
+            if (existingProduct == null)
+            {
+                return null; // Product not found
+            }
+
+            existingProduct.Name = updatedProduct.Name;
+            existingProduct.Description = updatedProduct.Description;
+
+            await db.SaveChangesAsync();
+
+            return existingProduct;
+        }
+    }
+}
